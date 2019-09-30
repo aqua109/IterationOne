ce77e086a61ae55319dae1722f51853338e26d32
Aidan Quayle <aqua109@aucklanduni.ac.nz>
2019-09-30 19:21:54 +1300

    Add script to prefab

--- Assets/Scripts/CreateCube.cs
+++ Assets/Scripts/CreateCube.cs
@@ -7,6 +7,6 @@ public class CreateCube : MonoBehaviour
 {
     public void InstantiateCube()
     {
-        PhotonNetwork.InstantiateSceneObject("Cube", new Vector3(0, 6, -16.5f), Quaternion.identity, 0, null);
+        var cube = PhotonNetwork.InstantiateSceneObject("Cube", new Vector3(0, 6, -16.5f), Quaternion.identity, 0, null);
     }
 }
