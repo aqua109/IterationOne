0bfd0d23336786832dd4d56ffe0f4213bf1b1bd6
Aidan Quayle <aqua109@aucklanduni.ac.nz>
2019-09-30 16:55:35 +1300

    Testing object sync across clients

---
+++ Assets/Scripts/PhotonViewTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Pathfinding.Serialization.JsonFx;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using TMPro;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PhotonViewTest : MonoBehaviour
+{
+    public void cube()
+    {
+        PhotonNetwork.InstantiateSceneObject("Cube", new Vector3(0, 6, -16.5f), Quaternion.identity, 0, null);
+    }
+}
