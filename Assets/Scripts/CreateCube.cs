252ec78b33848a7ad9ec7fba290c4d05151158e4
Aidan Quayle <aqua109@aucklanduni.ac.nz>
2019-10-02 18:47:15 +1300

    Working object and text syncing prototype

--- Assets/Scripts/CreateCube.cs
+++ Assets/Scripts/CreateCube.cs
@@ -7,17 +7,20 @@ using TMPro;
 public class CreateCube : MonoBehaviour
 {
     private int i = 0;
-    public GameObject data;
 
     public void InstantiateCube()
     {
-        //var cube = PhotonNetwork.InstantiateSceneObject("Cube", new Vector3(0, 6, -16.5f), Quaternion.identity, 0, null);
+        //PhotonView photonView = PhotonView.Get(this);
         var cube = PhotonNetwork.Instantiate("Cube", new Vector3(0, 6, -16.5f), Quaternion.identity, 0);
         cube.transform.Find("Canvas").GetComponentInChildren<TextMeshProUGUI>().SetText(i.ToString());
-        Debug.Log(cube.GetType());
-        var point = (GameObject)Instantiate(data, new Vector3(0, 7, -16), Quaternion.identity);
-        //point.transform.Find("Canvas").GetComponentInChildren<TextMeshProUGUI>().SetText(i.ToString());
-        Debug.Log(point.GetType());
+        //photonView.RPC("UpdateText", RpcTarget.All, i);
         i++;
+    }
+
+    [PunRPC]
+    void UpdateText(int i)
+    {
+        var cube = PhotonNetwork.Instantiate("Cube", new Vector3(0, 6, -16.5f), Quaternion.identity, 0);
+        cube.transform.Find("Canvas").GetComponentInChildren<TextMeshProUGUI>().SetText(i.ToString());
     }
 }
