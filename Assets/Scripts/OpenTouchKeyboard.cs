0757b08c81193b3ec24ccdf914384ea7d6ccd26c
Aidan Quayle <aqua109@aucklanduni.ac.nz>
2019-10-10 12:05:01 +1300

    Visual updates for various buttons

---
+++ Assets/Scripts/OpenTouchKeyboard.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenTouchKeyboard : MonoBehaviour
+{
+    public TouchScreenKeyboard keyboard;
+    string keyboardText;
+
+
+    public void OpenSystemKeyboard()
+    {
+        keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default, false, false, false, false, "Enter your name");
+    }
+    void Update()
+    {
+        if (TouchScreenKeyboard.visible == false && keyboard != null)
+        {
+            if (keyboard.done == true)
+            {
+                keyboardText = keyboard.text;
+                keyboard = null;
+                Debug.Log(keyboardText);
+            }
+        }
+    }
+}
