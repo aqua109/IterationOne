0c032ccca6313c7e287f90f5d1016678f926f5c8
Aidan <aqua109@aucklanduni.ac.nz>
2019-10-16 14:23:37 +1300

    Editing user podium

---
+++ Assets/Scripts/GroupMembersConnector.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+
+public class GroupMembersConnector : MonoBehaviour
+{
+    public TabletData[] tablets;
+
+    void Awake()
+    {
+        string url = "https://api.jsonbin.io/b/5da64aa05d7043458ee9334a";
+        WWW myWww = new WWW(url);
+        while (myWww.isDone == false) ;
+        string jsonResponse = myWww.text;
+
+        if (string.IsNullOrEmpty(jsonResponse))
+        {
+            return;
+        }
+        tablets = JsonConvert.DeserializeObject<TabletData[]>(jsonResponse);
+    }
+}
